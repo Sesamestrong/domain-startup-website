4ecac9deea4b1381d6f7a58ad8d7fa9a041c7150
Andrew Healey <doolie.healey@gmail.com>
2020-04-24 20:44:45 +0000

    Add frontend

---
+++ client.js
@@ -0,0 +1,35 @@
+import { resolve } from "dns";
+
+const display = document.querySelector("#display");
+const domainList = document.querySelector("#domainDisplay");
+const freenomUsername = document.querySelector("#freenomUsername");
+const freenomPassword = document.querySelector("#freenomPassword");
+async function setupDomain() {
+    const info = await new Promise((resolve) => {
+        const buh = setTimeout(() => fetch("https://api.domainstartup.ml/ping"), 1000 * 10);
+        (fetch("https://api.domainstartup.ml/setup", {
+            method: "POST",
+            body: new FormData(document.domainInfo)
+        }))
+            .then(b => {
+                clearTimeout(buh);
+                resolve(b.json())
+            });
+    });
+    domainList.innerHTML = "";
+    for (let { herokuAppId, id } of info.domains) {
+        const li = document.createElement("li");
+        const heroku = document.createElement("input");
+        const cloudflare = document.createElement("input");
+        heroku.value = herokuAppId;
+        cloudflare.value = id;
+        heroku.readOnly = true;
+        cloudflare.readOnly = true;
+        li.appendChild(heroku);
+        li.appendChild(cloudflare);
+        domainList.appendChild(li);
+    }
+    freenomUsername.value = info.freenom.username;
+    freenomPassword.value = info.freenom.password;
+    alert("Finished!");
+}
